a50cbaf4f908e8b8a3c905eafc266cd6ee58e821
Jay Elbourne <jayelbourne@gmail.com>
2018-10-17 11:02:17 -0400

    Added the basic character classes.

---
+++ NotARoguelike/PlayerCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotARoguelike
+{
+    class PlayerCharacter : Character
+    {
+        public PlayerCharacter()
+        {
+            _instance = this;
+        }
+
+        private static PlayerCharacter _instance;
+
+        static public PlayerCharacter Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
