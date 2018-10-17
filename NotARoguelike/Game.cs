e5b3c9a2c93231cfbce6732f9bbdbf3dced7bbf6
Jay Elbourne <jayelbourne@gmail.com>
2018-10-16 21:45:15 -0400

    Initial Commit

---
+++ NotARoguelike/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotARoguelike
+{
+    class Game
+    {
+        public Game()
+        {
+
+        }
+
+        public bool Restart = false;
+
+        public bool Update()
+        {
+            return true;
+        }
+    }
+}
--- NotARoguelike/Program.cs
+++ NotARoguelike/Program.cs
@@ -2,20 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NotARoguelike
 {
     class Program
     {
-        static void Main(string[] args)
+        class MainClass
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            public static void Main(string[] args)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+                while (RunGame())
+                {
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+                }
+
+                Console.ResetColor();
+                Console.Clear();
+            }
+
+            static bool RunGame()
+            {
+                Console.CursorVisible = false;
+
+                Game game = new Game();
+
+                //Intro intro = new Intro();
+                //intro.Play();
+
+                while (game.Update())
+                {
+                    // Tiny nap to stop the CPU from melting.
+                    Thread.Sleep(10);
+                }
+
+                return game.Restart;
+            }
         }
     }
 }
